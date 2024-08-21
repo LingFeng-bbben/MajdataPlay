f844363a14c712cb486be8a7e3593675c36b370e
LeZi <lezi@leziblog.com>
2024-08-21 22:45:34 +0800

    feat: deep copy - object extension method

--- Assets/Script/Misc/Types/Extensions.cs
+++ Assets/Script/Misc/Types/Extensions.cs
@@ -1,10 +1,57 @@
 ﻿using MajdataPlay.Types;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 #nullable enable
 namespace MajdataPlay.Extensions
 {
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// Deep copy
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static T? Clone<T>(this T? source)
+        {
+            if (source is null)
+                return default;
+            else if (source is string)
+                return source;
+
+            var type = source.GetType();
+
+            if (type.IsValueType)
+                return source;
+
+            if (type.IsArray)
+            {
+                var elementType = type.GetElementType();
+                var array = source as Array;
+                if (array is null)
+                    return default;
+                Array copiedArray = Array.CreateInstance(elementType, array.Length);
+
+                for (int i = 0; i < array.Length; i++)
+                    copiedArray.SetValue(array.GetValue(i).Clone(), i);
+
+                return (T)(object)copiedArray;
+            }
+
+            var result = Activator.CreateInstance(type);
+            var fields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            foreach (var field in fields)
+            {
+                object fieldValue = field.GetValue(source);
+                if (fieldValue != null)
+                    field.SetValue(result, fieldValue.Clone());
+            }
+
+            return (T)result;
+        }
+    }
     public static class IEnumerableExtensions
     {
         public static IEnumerable<(int, T)> WithIndex<T>(this IEnumerable<T> source)
