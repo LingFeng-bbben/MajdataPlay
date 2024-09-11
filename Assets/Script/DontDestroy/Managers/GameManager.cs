99a5535141465f2022d40e9d2396d848efc9050f
LeZi <lezi@leziblog.com>
2024-09-11 18:15:22 +0800

    fix: FullScreen

--- Assets/Script/DontDestroy/Managers/GameManager.cs
+++ Assets/Script/DontDestroy/Managers/GameManager.cs
@@ -87,8 +87,8 @@ public class GameManager : MonoBehaviour
         Setting.Display.OuterJudgeDistance = Setting.Display.OuterJudgeDistance.Clamp(1, 0);
 
         var fullScreen = Setting.Debug.FullScreen;
-        if (!fullScreen)
-            Screen.fullScreen = false;
+        Screen.fullScreen = fullScreen;
+
         var resolution = Setting.Display.Resolution.ToLower();
         if (resolution is not "auto")
         {
