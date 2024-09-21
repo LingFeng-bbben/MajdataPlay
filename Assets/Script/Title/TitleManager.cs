0b708f8ed459613e0939c7fc7bb3d2aed8124f1f
LeZi <lezi@leziblog.com>
2024-09-21 22:28:40 +0800

    misc: typo

--- Assets/Script/Title/TitleManager.cs
+++ Assets/Script/Title/TitleManager.cs
@@ -17,107 +17,116 @@ namespace MajdataPlay.Title
         public TextMeshProUGUI echoText;
         public Animator fadeInAnim;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        DelayPlayVoice().Forget();
-        WaitForScanningTask().Forget();
-        LightManager.Instance.SetAllLight(Color.white);
-    }
-    private void OnAreaDown(object sender, InputEventArgs e)
-    {
-        if (!e.IsClick)
-            return;
-        if(e.IsButton)
-            NextScene();
-        else
+        // Start is called before the first frame update
+        void Start()
         {
-            switch(e.Type)
-            {
-                case SensorType.A8:
-                    AudioManager.Instance.OpenAsioPannel();
-                    break;
-                case SensorType.E5:
-                    NextScene();
-                    break;
-            }
+            DelayPlayVoice().Forget();
+            WaitForScanningTask().Forget();
+            LightManager.Instance.SetAllLight(Color.white);
         }
-    }
-    async UniTaskVoid WaitForScanningTask()
-    {
-        bool isEmpty = false;
-        float animTimer = 0;
-        while(animTimer < 2f)
-        {
-            await UniTask.Yield(PlayerLoopTiming.Update);
-            animTimer += Time.deltaTime;
-            if (SongStorage.State == ComponentState.Finished)
-            {
-                if (SongStorage.IsEmpty)
-                {
-                    isEmpty = true;
-                    echoText.text = "No Charts";
-                    return;
-                }
-                else
-                {
-                    echoText.text = "Press Any Key";
-                    InputManager.Instance.BindAnyArea(OnAreaDown);
-                    return;
-                }
-            }
-        }
-        fadeInAnim.enabled = false;
-        int a = -1;
-        float timer = 2f;
-        while(true)
-        {
-            await UniTask.Yield(PlayerLoopTiming.Update);
-            var state = SongStorage.State;
-            if (timer >= 2f)
-                a = -1;
-            else if(timer == 0)
-                a = 1;
-            timer += Time.deltaTime * a;
-            timer = timer.Clamp(0, 2f);
-            var newColor = Color.white;
 
-            if (state >= ComponentState.Finished)
+        private void OnAreaDown(object sender, InputEventArgs e)
+        {
+            if (!e.IsClick)
+                return;
+            if (e.IsButton)
+                NextScene();
+            else
             {
-                if(state == ComponentState.Failed)
-                    echoText.text = "Scan Chart Failed";
-                else if(SongStorage.IsEmpty)
+                switch (e.Type)
                 {
-                    isEmpty = true;
-                    echoText.text = "No Charts";
-                }
-                else
-                    echoText.text = "Press Any Key";
-                if (timer >= 1.8f)
-                {
-                    echoText.color = newColor;
-                    break;
+                    case SensorType.A8:
+                        AudioManager.Instance.OpenAsioPannel();
+                        break;
+                    case SensorType.E5:
+                        NextScene();
+                        break;
                 }
             }
-            newColor.a = timer / 2f;
-            echoText.color = newColor;
         }
-        if (isEmpty)
-            return;
-        else if(SongStorage.State != ComponentState.Failed)
-            InputManager.Instance.BindAnyArea(OnAreaDown);
-    }
-    async UniTaskVoid DelayPlayVoice()
-    {
-        await UniTask.Yield(PlayerLoopTiming.LastPostLateUpdate);
-        AudioManager.Instance.PlaySFX(SFXSampleType.MAJDATA_PLAY);
-        AudioManager.Instance.PlaySFX(SFXSampleType.TITLE_BGM);
-    }
-    void NextScene()
-    {
-        InputManager.Instance.UnbindAnyArea(OnAreaDown);
-        AudioManager.Instance.StopSFX(SFXSampleType.TITLE_BGM);
-        AudioManager.Instance.StopSFX(SFXSampleType.MAJDATA_PLAY);
-        SceneManager.LoadSceneAsync(1);
+
+        async UniTaskVoid WaitForScanningTask()
+        {
+            bool isEmpty = false;
+            float animTimer = 0;
+            while (animTimer < 2f)
+            {
+                await UniTask.Yield(PlayerLoopTiming.Update);
+                animTimer += Time.deltaTime;
+                if (SongStorage.State == ComponentState.Finished)
+                {
+                    if (SongStorage.IsEmpty)
+                    {
+                        isEmpty = true;
+                        echoText.text = "No Charts";
+                        return;
+                    }
+                    else
+                    {
+                        echoText.text = "Press Any Key";
+                        InputManager.Instance.BindAnyArea(OnAreaDown);
+                        return;
+                    }
+                }
+            }
+
+            fadeInAnim.enabled = false;
+            int a = -1;
+            float timer = 2f;
+            while (true)
+            {
+                await UniTask.Yield(PlayerLoopTiming.Update);
+                var state = SongStorage.State;
+                if (timer >= 2f)
+                    a = -1;
+                else if (timer == 0)
+                    a = 1;
+                timer += Time.deltaTime * a;
+                timer = timer.Clamp(0, 2f);
+                var newColor = Color.white;
+
+                if (state >= ComponentState.Finished)
+                {
+                    if (state == ComponentState.Failed)
+                        echoText.text = "Scan Chart Failed";
+                    else if (SongStorage.IsEmpty)
+                    {
+                        isEmpty = true;
+                        echoText.text = "No Charts";
+                    }
+                    else
+                        echoText.text = "Press Any Key";
+
+                    if (timer >= 1.8f)
+                    {
+                        echoText.color = newColor;
+                        break;
+                    }
+                }
+
+                newColor.a = timer / 2f;
+                echoText.color = newColor;
+            }
+
+            if (isEmpty)
+                return;
+            else if (SongStorage.State != ComponentState.Failed)
+                InputManager.Instance.BindAnyArea(OnAreaDown);
+        }
+
+        async UniTaskVoid DelayPlayVoice()
+        {
+            await UniTask.Yield(PlayerLoopTiming.LastPostLateUpdate);
+            AudioManager.Instance.PlaySFX(SFXSampleType.MAJDATA_PLAY);
+            AudioManager.Instance.PlaySFX(SFXSampleType.TITLE_BGM);
+        }
+
+        void NextScene()
+        {
+            InputManager.Instance.UnbindAnyArea(OnAreaDown);
+            AudioManager.Instance.StopSFX(SFXSampleType.TITLE_BGM);
+            AudioManager.Instance.StopSFX(SFXSampleType.MAJDATA_PLAY);
+            SceneManager.LoadSceneAsync(1);
+        }
     }
 }
