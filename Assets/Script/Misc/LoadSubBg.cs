a8a33006d0e04c3a63a262644a7ef055d3cb37cb
LingFeng-bbben <bengy@126.com>
2024-10-10 19:08:45 +0800

    clean up

--- Assets/Script/Misc/LoadSubBg.cs
+++ Assets/Script/Misc/LoadSubBg.cs
@@ -11,6 +11,11 @@ namespace MajdataPlay.Misc
         // Start is called before the first frame update
         void Start()
         {
+            StartCoroutine(WaitSkinManager());
+        }
+        IEnumerator WaitSkinManager()
+        {
+            yield return new WaitForEndOfFrame();
             var img = GetComponent<Image>();
             img.sprite = SkinManager.Instance.SelectedSkin.SubDisplay;
             img.color = Color.white;
