54a7590b0828812190321893287e0c2558560dbb
LeZi <lezi@leziblog.com>
2024-10-17 00:27:02 +0800

    feat: DownloadResult

--- Assets/Script/Misc/Types/Net/DownloadResult.cs
+++ Assets/Script/Misc/Types/Net/DownloadResult.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using MajdataPlay.Extensions;
+using System;
+using System.IO;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +9,46 @@ namespace MajdataPlay.Net
 {
     public readonly struct DownloadResult
     {
+        public bool IsSuccess
+        {
+            get
+            {
+                var value = (int)StatusCode;
+                if (StatusCode is HttpStatusCode.NoContent)
+                    return false;
+                else
+                    return value.InRange(200, 299) && RequestError == HttpRequestError.NoError;
+            }
+        }
         public long Length { get; init; }
         public string SavePath { get; init; }
         public DateTime StartAt { get; init; }
         public HttpStatusCode StatusCode { get; init; }
         public HttpRequestError RequestError { get; init; }
+
+        public FileStream ReadAsStream()
+        {
+            ThrowIfFailed();
+            return File.OpenRead(SavePath);
+        }
+        public string ReadAsString()
+        {
+            ThrowIfFailed();
+            return File.ReadAllText(SavePath);
+        }
+        public async Task<string> ReadAsStringAsync()
+        {
+            ThrowIfFailed();
+            using var stream = ReadAsStream();
+            var buffer = new byte[stream.Length];
+            await stream.ReadAsync(buffer, 0, buffer.Length);
+
+            return Encoding.UTF8.GetString(buffer);
+        }
+        void ThrowIfFailed()
+        {
+            if (!IsSuccess)
+                throw new InvalidOperationException("This download operation was unsuccessful");
+        }
     }
 }
