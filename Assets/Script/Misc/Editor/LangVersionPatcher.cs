85b8b1a715d9f25b6ab31b27852038b48e6fbe80
LeZi9916 <lezi@leziblog.com>
2024-10-24 21:39:45 +0800

    misc: C# 10

---
+++ Assets/Script/Misc/Editor/LangVersionPatcher.cs
@@ -0,0 +1,56 @@
+﻿using System.IO;
+using System.Linq;
+using System.Reflection;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEngine;
+
+namespace MajdataPlay.Misc.Editor
+{
+    public class LangVersionPatcher : AssetPostprocessor
+    {
+        const string OldVersion = "9.0";
+        const string NewVersion = "10.0";
+
+        [InitializeOnLoadMethod]
+        public static void Setup()
+        {
+            var targets = typeof(NamedBuildTarget).GetFields(BindingFlags.Public | BindingFlags.Static)
+                                                  .Where(field => field.FieldType == typeof(NamedBuildTarget))
+                                                  .Select(field => (NamedBuildTarget)field.GetValue(null));
+
+            bool dirty = false;
+            foreach (NamedBuildTarget target in targets)
+            {
+                const string CscFlag = "-langversion:";
+
+                try
+                {
+                    var arguments = PlayerSettings.GetAdditionalCompilerArguments(target);
+                    if (arguments.Any(argument => argument.StartsWith(CscFlag)))
+                        continue;
+                    PlayerSettings.SetAdditionalCompilerArguments(target, arguments.Append(CscFlag + NewVersion).ToArray());
+                    dirty = true;
+                }
+                catch
+                {
+                    // ignore
+                }
+            }
+            if (!dirty)
+                return;
+            var projectPath = Path.GetDirectoryName(Path.GetFullPath(Application.dataPath));
+            foreach (var file in Directory.GetFiles(projectPath, "*.csproj"))
+                File.Delete(file);
+            foreach (var file in Directory.GetFiles(projectPath, "*.sln"))
+                File.Delete(file);
+            Debug.Log($"Patched C# version from {OldVersion} to {NewVersion}!");
+        }
+
+        public static string OnGeneratedCSProject(string path, string content)
+        {
+            const string MsBuildFlag = "LangVersion";
+            return content.Replace($"<{MsBuildFlag}>{OldVersion}</{MsBuildFlag}>", $"<{MsBuildFlag}>{NewVersion}</{MsBuildFlag}>");
+        }
+    }
+}
