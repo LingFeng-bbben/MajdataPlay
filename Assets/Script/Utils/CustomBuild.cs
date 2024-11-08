d06ab671fdc83f678a0ccccf74a8b8ea63aa6a6d
LeZi9916 <lezi@leziblog.com>
2024-11-08 18:58:12 +0800

    feat: CustomBuild

---
+++ Assets/Script/Utils/CustomBuild.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+
+namespace MajdataPlay.Utils
+{
+    public static class CustomBuild
+    {
+        public static void BuildDebug()
+        {
+            var buildPlayerOptions = new BuildPlayerOptions
+            {
+                scenes = GetScenesToBuild(),
+                options = BuildOptions.Development | BuildOptions.AllowDebugging
+            };
+            BuildPipeline.BuildPlayer(buildPlayerOptions);
+        }
+        static string[] GetScenesToBuild()
+        {
+            var sceneCount = EditorBuildSettings.scenes.Length;
+            var scenes = new string[sceneCount];
+
+            for (int i = 0; i < sceneCount; i++)
+                scenes[i] = EditorBuildSettings.scenes[i].path;
+
+            return scenes;
+        }
+    }
+}
