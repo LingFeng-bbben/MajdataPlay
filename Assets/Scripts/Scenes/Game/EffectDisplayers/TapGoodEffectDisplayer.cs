c2a3ed0b8450bfc1ca921fdb7507fda763c81628
LeZi9916 <lezi@leziblog.com>
2025-02-23 01:52:20 +0800

    perf: TapEffect

---
+++ Assets/Scripts/Scenes/Game/EffectDisplayers/TapGoodEffectDisplayer.cs
@@ -0,0 +1,81 @@
+﻿using MajdataPlay.Extensions;
+using MajdataPlay.Types;
+using MajdataPlay.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace MajdataPlay.Game
+{
+    internal sealed class TapGoodEffectDisplayer : MajComponent
+    {
+        Animator _animator;
+        GameObject[] _children = Array.Empty<GameObject>();
+
+        static readonly int TAP_GOOD_ANIM_HASH = Animator.StringToHash("good");
+        protected override void Awake()
+        {
+            base.Awake();
+            _animator = GameObject.GetComponent<Animator>();
+            _children = Transform.GetChildren()
+                                 .Select(x => x.gameObject)
+                                 .ToArray();
+            SetActiveInternal(false);
+        }
+        public void Reset()
+        {
+            SetActive(false);
+        }
+        public void PlayEffect(in JudgeResult judgeResult)
+        {
+            if (judgeResult.IsBreak)
+            {
+                return;
+            }
+            var grade = judgeResult.Grade;
+            switch (grade)
+            {
+                case JudgeGrade.LateGood:
+                case JudgeGrade.FastGood:
+                    SetActive(true);
+                    _animator.SetTrigger(TAP_GOOD_ANIM_HASH);
+                    break;
+            }
+        }
+        public override void SetActive(bool state)
+        {
+            if (Active == state)
+                return;
+            SetActiveInternal(state);
+        }
+        void SetActiveInternal(bool state)
+        {
+            Active = state;
+            base.SetActive(state);
+            switch (state)
+            {
+                case true:
+                    foreach (var child in ArrayHelper.ToEnumerable(_children))
+                    {
+                        if (child is null)
+                            continue;
+                        child.layer = MajEnv.DEFAULT_LAYER;
+                    }
+                    GameObject.layer = MajEnv.DEFAULT_LAYER;
+                    break;
+                case false:
+                    foreach (var child in ArrayHelper.ToEnumerable(_children))
+                    {
+                        if (child is null)
+                            continue;
+                        child.layer = MajEnv.HIDDEN_LAYER;
+                    }
+                    GameObject.layer = MajEnv.HIDDEN_LAYER;
+                    break;
+            }
+        }
+    }
+}
