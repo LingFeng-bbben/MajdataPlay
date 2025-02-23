297ec76b71d50cdc7ba45b94bbf532782734144c
LeZi9916 <lezi@leziblog.com>
2025-02-23 17:17:02 +0800

    perf: TouchEffect

--- Assets/Scripts/Scenes/Game/EffectDisplayers/TouchFeedbackDisplayer.cs
+++ Assets/Scripts/Scenes/Game/EffectDisplayers/TouchFeedbackDisplayer.cs
@@ -1,30 +1,52 @@
-﻿using System;
+﻿using MajdataPlay.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using static Unity.VisualScripting.Metadata;
 #nullable enable
 namespace MajdataPlay.Game
 {
-    public sealed class TouchFeedbackDisplayer: MonoBehaviour
+    internal sealed class TouchFeedbackDisplayer : MajComponent
     {
         [SerializeField]
         Animator _anim;
         readonly int _id = Animator.StringToHash("Triggered");
-        GameObject _gameObject;
-        private void Awake()
+        protected override void Awake()
         {
-            _gameObject = gameObject;
+            base.Awake();
+            SetActiveInternal(false);
         }
         public void Reset()
         {
-            _gameObject.SetActive(false);
+            SetActive(false);
         }
         public void Play()
         {
-            _gameObject.SetActive(true);
+            SetActive(true);
             _anim.SetTrigger(_id);
+        }
+        public override void SetActive(bool state)
+        {
+            if (Active == state)
+                return;
+            SetActiveInternal(state);
+        }
+        void SetActiveInternal(bool state)
+        {
+            Active = state;
+            base.SetActive(state);
+            switch (state)
+            {
+                case true:
+                    GameObject.layer = MajEnv.DEFAULT_LAYER;
+                    break;
+                case false:
+                    GameObject.layer = MajEnv.HIDDEN_LAYER;
+                    break;
+            }
         }
     }
 }
