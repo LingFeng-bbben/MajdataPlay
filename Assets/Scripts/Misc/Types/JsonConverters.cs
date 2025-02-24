87fc01e046ac9e28c9224e588e4481266da3771d
LeZi9916 <lezi@leziblog.com>
2025-02-24 17:14:40 +0800

    fix

--- Assets/Scripts/Misc/Types/JsonConverters.cs
+++ Assets/Scripts/Misc/Types/JsonConverters.cs
@@ -21,11 +21,45 @@ namespace MajdataPlay.Types
     {
         public override JudgeInfo Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            var dict = JsonSerializer.Deserialize<Dictionary<JudgeGrade, int>>(ref reader, options);
-            foreach(JudgeGrade judgeResult in Enum.GetValues(typeof(JudgeGrade)))
+            var data = JsonSerializer.Deserialize<Dictionary<string, int>>(ref reader, options);
+            var dict = new Dictionary<JudgeGrade, int>();
+            var type = typeof(JudgeGrade);
+            foreach (JudgeGrade judgeResult in Enum.GetValues(type))
             {
-                if (!dict.TryGetValue(judgeResult, out int i))
+                if (!data.TryGetValue(Enum.GetName(type, judgeResult), out int i))
                     dict[judgeResult] = 0;
+                else 
+                    dict[judgeResult] = i;
+            }
+            foreach (var (k, v) in data)
+            {
+                switch(k)
+                {
+                    case "FastGreat1":
+                        dict[JudgeGrade.FastGreat2nd] = v;
+                        break;
+                    case "FastGreat2":
+                        dict[JudgeGrade.FastGreat3rd] = v;
+                        break;
+                    case "LateGreat1":
+                        dict[JudgeGrade.LateGreat2nd] = v;
+                        break;
+                    case "LateGreat2":
+                        dict[JudgeGrade.LateGreat3rd] = v;
+                        break;
+                    case "FastPerfect1":
+                        dict[JudgeGrade.FastPerfect2nd] = v;
+                        break;
+                    case "FastPerfect2":
+                        dict[JudgeGrade.FastPerfect3rd] = v;
+                        break;
+                    case "LatePerfect1":
+                        dict[JudgeGrade.LatePerfect2nd] = v;
+                        break;
+                    case "LatePerfect2":
+                        dict[JudgeGrade.LatePerfect3rd] = v;
+                        break;
+                }
             }
             return new JudgeInfo(dict);
         }
