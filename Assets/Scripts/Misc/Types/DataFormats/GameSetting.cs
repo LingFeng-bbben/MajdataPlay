edad2457061fe18d28be2333476a2227c88218fd
LeZi9916 <lezi@leziblog.com>
2025-02-28 01:41:32 +0800

    perf: default setting

--- Assets/Scripts/Misc/Types/DataFormats/GameSetting.cs
+++ Assets/Scripts/Misc/Types/DataFormats/GameSetting.cs
@@ -143,11 +143,11 @@ namespace MajdataPlay.Types
         public bool TryFixAudioSync { get; set; } = false;
         public float NoteAppearRate { get; set; } = 0.265f;
         public bool DisableGCInGameing { get; set; } = true;
-        public int TapPoolCapacity { get; set; } = 128;
-        public int HoldPoolCapacity { get; set; } = 64;
+        public int TapPoolCapacity { get; set; } = 96;
+        public int HoldPoolCapacity { get; set; } = 48;
         public int TouchPoolCapacity { get; set; } = 64;
-        public int TouchHoldPoolCapacity { get; set; } = 64;
-        public int EachLinePoolCapacity { get; set; } = 96;
+        public int TouchHoldPoolCapacity { get; set; } = 16;
+        public int EachLinePoolCapacity { get; set; } = 64;
     }
     public class MiscOptions
     {
