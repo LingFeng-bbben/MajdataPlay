4d6a4ea2e179451ff2868d6b13c9cae92f6af661
LingFeng-bbben <bengy@126.com>
2025-03-05 22:34:42 +0800

    feat: xxlb

---
+++ Assets/Scripts/Scenes/Title/XxlbBlink.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class XxlbBlink : MonoBehaviour
+{
+    public Sprite XxlbDefault;
+    public Sprite XxlbBlinks;
+    public Image image;
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        var ran = Random.Range(0, 80);
+        if(ran == 1)
+        {
+            image.sprite = XxlbBlinks;
+        }
+        else
+        {
+            image.sprite = XxlbDefault;
+        }
+    }
+}
