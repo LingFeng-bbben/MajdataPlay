7c4c482cf735bde107c5c7ffca2b7ccd7f483cb3
KirisameVanilla <vanillaaaajp@gmail.com>
2025-03-27 09:44:58 +0800

    feat: websocket message receive

--- Assets/Scripts/Utils/OBSRecordHelper.cs
+++ Assets/Scripts/Utils/OBSRecordHelper.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using MajdataPlay.Types;
+﻿using MajdataPlay.Types;
+using System;
 using WebSocketSharp;
 
 namespace MajdataPlay.Utils
@@ -33,24 +33,37 @@ namespace MajdataPlay.Utils
                     }
                 }";
 
-        public OBSRecordHelper()
-        {
-            Connect();
-            Authenticate();
-        }
+        public OBSRecordHelper() => Init();
 
+        public void Init()
+        {
+            try
+            {
+                webSocket.OnMessage += OnMessageReceived;
+                Connect();
+                Authenticate();
+            }
+            catch (Exception e)
+            {
+                MajDebug.LogException(e);
+            }
+        }
         private void Connect()
         {
             webSocket.Connect();
-            // TODO: 根据消息返回来确认是否已连接
-            Connected = true;
         }
 
         private void Disconnect()
         {
-            webSocket.Close();
-            // TODO: 根据消息返回来确认是否已连接
-            Connected = false;
+            try
+            {
+                webSocket.Close();
+                Connected = false;
+            }
+            catch (Exception e)
+            {
+                MajDebug.LogException(e);
+            }
         }
 
         public void Dispose()
@@ -59,19 +72,115 @@ namespace MajdataPlay.Utils
         }
 
         public void StartRecord()
-        {
+        { 
             webSocket.Send(StartRecordMessage);
-            // TODO: 根据消息返回来确认是否已开始录制
-            Recording = true;
         }
 
         public void StopRecord()
         {
             webSocket.Send(StopRecordMessage);
-            // TODO: 根据消息返回来确认是否已停止录制
-            Recording = false;
+        }
+
+        private void OnMessageReceived(object sender, MessageEventArgs e)
+        {
+            try
+            {
+                var message = Serializer.Json.Deserialize<ReceivedMessage>(e.Data);
+                switch (message.Op)
+                {
+                    case 2: // Identified
+                    {
+                        Connected = true;
+                        break;
+                    }
+                    case 7: // RequestResponse
+                    {
+                        if (message.D.RequestType == "StartRecord")
+                        {
+                            if (message.D.RequestStatus.Result)
+                            {
+                                Recording = true;
+                            }
+                            else
+                            {
+                                MajDebug.Log("Start Record Failed.");
+                            }
+                        }
+
+                        if (message.D.RequestType == "StopRecord")
+                        {
+                            if (message.D.RequestStatus.Result)
+                            {
+                                Recording = false;
+                                MajDebug.Log("Record Saved To " + message.D.ResponseData.OutputPath);
+                            }
+                            else
+                            {
+                                MajDebug.Log("Stop Record Failed.");
+                            }
+                        }
+
+                        break;
+                    }
+                    default:
+                    {
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MajDebug.LogException(ex);
+            }
         }
 
         private void Authenticate() => webSocket.Send(AuthenticateMessage);
+
+        #region WebSocketMessageClass
+        public class RequestStatus
+        {
+            public int Code { get; set; }
+            public bool Result { get; set; }
+        }
+
+        public class ResponseData
+        {
+            public string OutputPath { get; set; }
+        }
+
+        public class EventData
+        {
+            public bool OutputActive { get; set; }
+            public string OutputPath { get; set; }
+            public string OutputState { get; set; }
+        }
+
+        public class Event
+        {
+            public EventData EventData { get; set; }
+            public int EventIntent { get; set; }
+            public string EventType { get; set; }
+        }
+
+        public class DData
+        {
+            public string ObsWebSocketVersion { get; set; }
+            public int RpcVersion { get; set; }
+            public int NegotiatedRpcVersion { get; set; }
+            public string RequestId { get; set; }
+            public RequestStatus RequestStatus { get; set; }
+            public string RequestType { get; set; }
+            public EventData EventData { get; set; }
+            public string OutputPath { get; set; }
+            public Event Event { get; set; }
+            public ResponseData ResponseData { get; set; }
+        }
+
+        public class ReceivedMessage
+        {
+            public DData D { get; set; }
+            public int Op { get; set; }
+        }
+        #endregion
     }
 }
