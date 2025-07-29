8d86545b20c8b55ef5f5677677238af284110719
LeZi9916 <lezi@leziblog.com>
2025-07-29 15:45:08 +0800

    fix: display offset

--- Assets/Scripts/Scenes/Setting/Option.cs
+++ Assets/Scripts/Scenes/Setting/Option.cs
@@ -178,7 +178,7 @@ namespace MajdataPlay.Scenes.Setting
                             else
                             {
                                 _maxValue = null;
-                                _minValue = null;
+                                _minValue = 0;
                                 _step = 0.1m;
                             }
                         }
