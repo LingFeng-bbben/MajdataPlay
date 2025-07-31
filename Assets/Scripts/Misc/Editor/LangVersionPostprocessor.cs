c4b065db65760a6cd008291273b4dd82e0fdbb0b
LeZi9916 <lezi@leziblog.com>
2025-07-31 11:27:17 +0800

    chore: using C# preview version

---
+++ Assets/Scripts/Misc/Editor/LangVersionPostprocessor.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+using UnityEditor;
+
+namespace MajdataPlay.Editor
+{
+    internal sealed class LangVersionPostprocessor : AssetPostprocessor
+    {
+        private static string OnGeneratedCSProject(string path, string content)
+        {
+            var pattern = @"<LangVersion>(.*?)<\/LangVersion>";
+            var replacement = "<LangVersion>preview</LangVersion>";
+            content = Regex.Replace(content, pattern, replacement);
+            return content;
+        }
+    }
+}
