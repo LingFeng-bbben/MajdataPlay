f4fc901eab42edb2e9403cd17650d81201602dc6
LeZi9916 <lezi@leziblog.com>
2025-09-12 15:27:33 +0800

    feat: ILedUpdateFunction

---
+++ Assets/Scripts/IO/Base/ILedUpdateFunction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace MajdataPlay.IO;
+public interface ILedUpdateFunction
+{
+    Color CurrentColor { get; }
+
+    void Update(float deltaMs);
+    void Reset();
+}
