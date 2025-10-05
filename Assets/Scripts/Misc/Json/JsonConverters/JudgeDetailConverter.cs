7dfb08146841d7368f4c8f8bcee691d051fca1a6
LeZi9916 <lezi@leziblog.com>
2025-10-05 20:58:37 +0800

    wip: migrate to Newtonsoft.Json

--- Assets/Scripts/Misc/Json/JsonConverters/JudgeDetailConverter.cs
+++ Assets/Scripts/Misc/Json/JsonConverters/JudgeDetailConverter.cs
@@ -1,21 +1,39 @@
 ﻿using MajdataPlay.Collections;
 using MajdataPlay.Scenes.Game;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 
 namespace MajdataPlay.Json
 {
-    public class JudgeDetailConverter : JsonConverter<JudgeDetail>
+    public class JudgeDetailConverter : System.Text.Json.Serialization.JsonConverter<JudgeDetail>
     {
         public override JudgeDetail Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            var dict = JsonSerializer.Deserialize<Dictionary<ScoreNoteType, JudgeInfo>>(ref reader, options);
+            var dict = System.Text.Json.JsonSerializer.Deserialize<Dictionary<ScoreNoteType, JudgeInfo>>(ref reader, options);
 
             return new JudgeDetail(dict);
         }
 
-        public override void Write(Utf8JsonWriter writer, JudgeDetail value, JsonSerializerOptions options) => JsonSerializer.Serialize(writer, value, options);
+        public override void Write(Utf8JsonWriter writer, JudgeDetail value, JsonSerializerOptions options) => System.Text.Json.JsonSerializer.Serialize(writer, value, options);
+    }
+}
+namespace MajdataPlay.Json.Migrating
+{
+    public class JudgeDetailConverter : Newtonsoft.Json.JsonConverter<JudgeDetail>
+    {
+        public override JudgeDetail ReadJson(JsonReader reader, Type objectType, JudgeDetail? existingValue, bool hasExistingValue, Newtonsoft.Json.JsonSerializer serializer)
+        {
+            var obj = JObject.Load(reader);
+            var dict = obj.ToObject<Dictionary<ScoreNoteType, JudgeInfo>>(serializer);
+            return new JudgeDetail(dict);
+        }
+
+        public override void WriteJson(JsonWriter writer, JudgeDetail value, Newtonsoft.Json.JsonSerializer serializer)
+        {
+            serializer.Serialize(writer, value);
+        }
     }
 }
--- Assets/Scripts/Misc/Json/JsonConverters/JudgeInfoConverter.cs
+++ Assets/Scripts/Misc/Json/JsonConverters/JudgeInfoConverter.cs
@@ -1,5 +1,7 @@
 ﻿using MajdataPlay.Collections;
 using MajdataPlay.Scenes.Game.Notes;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Text.Json;
@@ -7,19 +9,23 @@ using System.Text.Json.Serialization;
 
 namespace MajdataPlay.Json
 {
-    public class JudgeInfoConverter : JsonConverter<JudgeInfo>
+    public class JudgeInfoConverter : System.Text.Json.Serialization.JsonConverter<JudgeInfo>
     {
         public override JudgeInfo Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            var data = JsonSerializer.Deserialize<Dictionary<string, int>>(ref reader, options);
+            var data = System.Text.Json.JsonSerializer.Deserialize<Dictionary<string, int>>(ref reader, options);
             var dict = new Dictionary<JudgeGrade, int>();
             var type = typeof(JudgeGrade);
             foreach (JudgeGrade judgeResult in Enum.GetValues(type))
             {
                 if (!data.TryGetValue(Enum.GetName(type, judgeResult), out var i))
+                {
                     dict[judgeResult] = 0;
+                }
                 else
+                {
                     dict[judgeResult] = i;
+                }
             }
             foreach (var (k, v) in data)
             {
@@ -54,6 +60,72 @@ namespace MajdataPlay.Json
             return new JudgeInfo(dict);
         }
 
-        public override void Write(Utf8JsonWriter writer, JudgeInfo value, JsonSerializerOptions options) => JsonSerializer.Serialize(writer, value, options);
+        public override void Write(Utf8JsonWriter writer, JudgeInfo value, JsonSerializerOptions options)
+        {
+            return System.Text.Json.JsonSerializer.Serialize(writer, value, options);
+        }
+    }
+}
+namespace MajdataPlay.Json.Migrating
+{
+    public class JudgeInfoConverter : Newtonsoft.Json.JsonConverter<JudgeInfo>
+    {
+        public override JudgeInfo ReadJson(JsonReader reader, Type objectType, JudgeInfo existingValue, bool hasExistingValue, Newtonsoft.Json.JsonSerializer serializer)
+        {
+            var data = JObject.Load(reader).ToObject<Dictionary<string, int>>(serializer);
+
+            var dict = new Dictionary<JudgeGrade, int>();
+            var type = typeof(JudgeGrade);
+
+            foreach (JudgeGrade judgeResult in Enum.GetValues(type))
+            {
+                if (!data.TryGetValue(Enum.GetName(type, judgeResult), out var i))
+                {
+                    dict[judgeResult] = 0;
+                }
+                else
+                {
+                    dict[judgeResult] = i;
+                }
+            }
+
+            foreach (var kv in data)
+            {
+                switch (kv.Key)
+                {
+                    case "FastGreat1":
+                        dict[JudgeGrade.FastGreat2nd] = kv.Value;
+                        break;
+                    case "FastGreat2":
+                        dict[JudgeGrade.FastGreat3rd] = kv.Value;
+                        break;
+                    case "LateGreat1":
+                        dict[JudgeGrade.LateGreat2nd] = kv.Value;
+                        break;
+                    case "LateGreat2":
+                        dict[JudgeGrade.LateGreat3rd] = kv.Value;
+                        break;
+                    case "FastPerfect1":
+                        dict[JudgeGrade.FastPerfect2nd] = kv.Value;
+                        break;
+                    case "FastPerfect2":
+                        dict[JudgeGrade.FastPerfect3rd] = kv.Value;
+                        break;
+                    case "LatePerfect1":
+                        dict[JudgeGrade.LatePerfect2nd] = kv.Value;
+                        break;
+                    case "LatePerfect2":
+                        dict[JudgeGrade.LatePerfect3rd] = kv.Value;
+                        break;
+                }
+            }
+
+            return new JudgeInfo(dict);
+        }
+
+        public override void WriteJson(JsonWriter writer, JudgeInfo value, Newtonsoft.Json.JsonSerializer serializer)
+        {
+            serializer.Serialize(writer, value);
+        }
     }
 }
