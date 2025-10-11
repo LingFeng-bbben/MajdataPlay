211a38266c065c83e8f857141358695d7227fa6b
LeZi9916 <lezi@leziblog.com>
2025-10-11 17:28:22 +0800

    feat: UnityWebRequestFactory

--- Assets/Scripts/Misc/Net/Exceptions/HttpException.cs
+++ Assets/Scripts/Misc/Net/Exceptions/HttpException.cs
@@ -5,43 +5,77 @@ using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEditor.PackageManager;
 #nullable enable
 namespace MajdataPlay.Net
 {
     internal class HttpException : HttpRequestException
     {
-        public HttpErrorCode ErrorCode { get; init; }
-        public HttpStatusCode? StatusCode { get; init; }
-        public HttpException()
-        {
+        public HttpErrorCode ErrorCode { get; }
+        public HttpStatusCode? StatusCode { get; }
 
-        }
-        public HttpException(string message) : base(message)
-        {
-            
-        }
-        public HttpException(string message, Exception inner) : base(message, inner)
-        {
+        readonly string _errMsg;
 
-        }
-        public HttpException(HttpErrorCode errorCode) : this(errorCode, null)
+        public HttpException(HttpErrorCode errorCode, string message) : base(message)
         {
-            
+            ErrorCode = errorCode;
+            _errMsg = message;
+        }
+        public HttpException(HttpErrorCode errorCode, string message, Exception inner) : base(message, inner)
+        {
+            ErrorCode = errorCode;
+            _errMsg = message;
+        }
+        public HttpException(HttpErrorCode errorCode)
+        {
+            ErrorCode = errorCode;
+            _errMsg = $"An error occurred while sending the HTTP request:\nErrorCode: {errorCode}";
         }
         public HttpException(HttpErrorCode errorCode, HttpStatusCode? statusCode) : base()
         {
             ErrorCode = errorCode;
             StatusCode = statusCode;
+
+            if (statusCode is not null)
+            {
+                _errMsg = $"An error occurred while sending the HTTP request:\nErrorCode: {errorCode}\nStatusCode: {statusCode}";
+            }
+            else
+            {
+                _errMsg = $"An error occurred while sending the HTTP request:\nErrorCode: {errorCode}";
+            }
         }
         public HttpException(HttpErrorCode errorCode, HttpStatusCode? statusCode, string message) : base(message)
         {
             ErrorCode = errorCode;
             StatusCode = statusCode;
+
+            if (statusCode is not null)
+            {
+                _errMsg = $"An error occurred while sending the HTTP request:\nErrorCode: {errorCode}\nStatusCode: {statusCode}";
+            }
+            else
+            {
+                _errMsg = $"An error occurred while sending the HTTP request:\nErrorCode: {errorCode}";
+            }
         }
         public HttpException(HttpErrorCode errorCode, HttpStatusCode? statusCode, string message, Exception inner) : base(message, inner)
         {
             ErrorCode = errorCode;
             StatusCode = statusCode;
+
+            if (statusCode is not null)
+            {
+                _errMsg = $"An error occurred while sending the HTTP request:\nErrorCode: {errorCode}\nStatusCode: {statusCode}";
+            }
+            else
+            {
+                _errMsg = $"An error occurred while sending the HTTP request:\nErrorCode: {errorCode}";
+            }
+        }
+        public override string ToString()
+        {
+            return _errMsg;
         }
     }
 }
