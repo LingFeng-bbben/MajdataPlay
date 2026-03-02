2594cbb48009045d6b679aa9f8946acf31ca9735
LeZi9916 <lezi@leziblog.com>
2026-03-02 23:24:09 +0800

    wip

--- Assets/Scripts/Misc/ScreenPos/ScreenPosCameraMover.cs
+++ Assets/Scripts/Misc/ScreenPos/ScreenPosCameraMover.cs
@@ -8,14 +8,14 @@ namespace MajdataPlay
 {
     public class ScreenPosCameraMover : MonoBehaviour
     {
-        //const int FLAG_NOT_INIT = 0;
-        //const int FLAG_INITED = 1;
+        const int FLAG_NOT_INIT = 0;
+        const int FLAG_INITED = 1;
 
-        Camera cam;
-        //int _flag = FLAG_NOT_INIT;
+        Camera _cam;
+        int _flag = FLAG_NOT_INIT;
         //float _lastMainScreenPos = 1f;
 
-        //Transform _transform;
+        Transform _transform;
         DisplayOptions? _displayOptions;
         
         float _baseY;
@@ -29,61 +29,25 @@ namespace MajdataPlay
         void Awake()
         {
             _displayOptions = MajInstances.Settings?.Display;
+            _transform = transform;
+            _cam = GetComponent<Camera>();
         }
-        void Start()
-        {
-            StartCoroutine(delayedStart());
-        }
-        
-        IEnumerator delayedStart()
-        {
-            _displayOptions = MajInstances.Settings?.Display;
-            while (_displayOptions is null)
-            {
-                yield return new WaitForEndOfFrame();
-                _displayOptions = MajInstances.Settings?.Display;
-            }
-
-            cam = GetComponent<Camera>();
-            _originalCameraY = transform.position.y; // 保存场景原始相机位置
-            _baseY = 1.5f;
-            //获取当前正交视图比例
-            _baseOrthographicSize = cam.orthographicSize;
-            ApplyTransform();
-            //transform.position = new Vector3(0, 1.5f + 2.7f * (MajInstances.Settings?.Display.MainScreenPosition ?? 1f), -10); //Original
-
-
-            cam = GetComponent<Camera>();
-            var aspectratio = (float)Screen.width / (float)Screen.height;
-
-
-            if (aspectratio < (9f / 18f))
-            {
-                cam.rect = new Rect(0, 0.22f, 1, 1);
-            }
-            else if (aspectratio < (9f / 16f))
-            {
-                cam.rect = new Rect(0, 0.12f, 1, 1);
-            }
-            else
-            {
-                cam.rect = new Rect(0, 0, 1, 1);
-            }
-            yield return new WaitForEndOfFrame();
-        }
-
         void RestoreOriginal()
         {
-            transform.position = new Vector3(0, _originalCameraY, -10);
-            if (cam.orthographic)
-                cam.orthographicSize = _baseOrthographicSize;
+            _transform.position = new Vector3(0, _originalCameraY, -10);
+            if (_cam.orthographic)
+            {
+                _cam.orthographicSize = _baseOrthographicSize;
+            }
         }
 
         void ApplyPosition(float offset, float scale)
         {
-            transform.position = new Vector3(0, _baseY + offset * 2.7f, -10);
-            if (scale > 0 && cam.orthographic)
-                cam.orthographicSize = _baseOrthographicSize / scale;
+            _transform.position = new Vector3(0, _baseY + offset * 2.7f, -10);
+            if (scale > 0 && _cam.orthographic)
+            {
+                _cam.orthographicSize = _baseOrthographicSize / scale;
+            }
         }
 
         void ApplyTransform()
@@ -106,62 +70,80 @@ namespace MajdataPlay
 
         void Update()
         {
-            if(_displayOptions is null) return;
-            var transformDisplay = _displayOptions.MainScreenTransform;
-
-            //如果没开调整显示位置，就直接return
-            if (!transformDisplay)
-            {
-                if (_lastTransformDisplay)
-                {
-                    _lastTransformDisplay = false;
-                    _lastOffset = float.NaN;
-                    _lastScale = float.NaN;
-                    RestoreOriginal();
-                }
-                return;
-            }
-            
-            //跑到这就是开了，标记开启了调整显示位置
-            _lastTransformDisplay = true;
-            
-            var screenOffset = _displayOptions.MainScreenPosition;
-            var screenScale = _displayOptions.MainScreenScale;
-
-            if (screenOffset == _lastOffset && screenScale == _lastScale) return;
-            _lastOffset = screenOffset;
-            _lastScale = screenScale;
-
-            //变换位置
-            ApplyPosition(screenOffset, screenScale);
-            
-            
-            
-            /*
             switch (_flag)
             {
                 case FLAG_NOT_INIT:
-                    if (_displayOptions is null)
                     {
-                        _displayOptions = MajInstances.Settings?.Display;
                         if (_displayOptions is null)
                         {
-                            return;
+                            _displayOptions = MajInstances.Settings?.Display;
+                            if (_displayOptions is null)
+                            {
+                                return;
+                            }
+                            _flag = FLAG_INITED;
+                            _originalCameraY = transform.position.y; // 保存场景原始相机位置
+                            _baseY = 1.5f;
+                            //获取当前正交视图比例
+                            _baseOrthographicSize = _cam.orthographicSize;
+                            ApplyTransform();
+                            //transform.position = new Vector3(0, 1.5f + 2.7f * (MajInstances.Settings?.Display.MainScreenPosition ?? 1f), -10); //Original
+
+
+                            var aspectratio = (float)Screen.width / (float)Screen.height;
+
+
+                            if (aspectratio < (9f / 18f))
+                            {
+                                _cam.rect = new Rect(0, 0.22f, 1, 1);
+                            }
+                            else if (aspectratio < (9f / 16f))
+                            {
+                                _cam.rect = new Rect(0, 0.12f, 1, 1);
+                            }
+                            else
+                            {
+                                _cam.rect = new Rect(0, 0, 1, 1);
+                            }
+                            goto case FLAG_INITED;
                         }
-                        _flag = FLAG_INITED;
-                        goto case FLAG_INITED;
                     }
                     return;
                 case FLAG_INITED:
-                    var pos = _displayOptions!.MainScreenPosition;
-                    if (_lastMainScreenPos == pos)
                     {
-                        return;
+                        var transformDisplay = _displayOptions!.MainScreenTransform;
+
+                        //如果没开调整显示位置，就直接return
+                        if (!transformDisplay)
+                        {
+                            if (_lastTransformDisplay)
+                            {
+                                _lastTransformDisplay = false;
+                                _lastOffset = float.NaN;
+                                _lastScale = float.NaN;
+                                RestoreOriginal();
+                            }
+                            return;
+                        }
+
+                        //跑到这就是开了，标记开启了调整显示位置
+                        _lastTransformDisplay = true;
+
+                        var screenOffset = _displayOptions.MainScreenPosition;
+                        var screenScale = _displayOptions.MainScreenScale;
+
+                        if (screenOffset == _lastOffset && screenScale == _lastScale)
+                        {
+                            return;
+                        }
+                        _lastOffset = screenOffset;
+                        _lastScale = screenScale;
+
+                        //变换位置
+                        ApplyPosition(screenOffset, screenScale);
                     }
-                    _lastMainScreenPos = pos;
-                    _transform.position = new Vector3(0, 1.5f + 2.7f * pos, -10);
                     return;
-            }*/
+            }
         }
     }
 }
