6198f86dc6277dbbc3a1249b2020aae3e458ed45
LeZi9916 <lezi@leziblog.com>
2026-03-07 02:03:30 +0800

    fix: ranking displayer

--- Assets/Scripts/Scenes/List/RankingDisplayer.cs
+++ Assets/Scripts/Scenes/List/RankingDisplayer.cs
@@ -93,7 +93,7 @@ namespace MajdataPlay
                 ref readonly var score = ref scores[i];
                 PlayerNames[i].text = string.Format(NameTemplates[i], score.Player.Username);
                 var @int = MathF.Truncate(score.Acc);
-                var @float = (int)((score.Acc - @int) * 1000);
+                var @float = (int)((score.Acc - @int) * 10000);
                 var comboState = CombostateToStr(score.ComboState);
                 Scores[i].text = string.Format(ScoresTemplate, @int, @float, comboState);
             }
