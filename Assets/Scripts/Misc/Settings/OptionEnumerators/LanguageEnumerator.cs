e8a4927cd1f0b4962e56941ec3fa5da1a73c3d49
LeZi9916 <lezi@leziblog.com>
2026-03-07 16:29:45 +0800

    chore: move

--- Assets/Scripts/Misc/Settings/OptionEnumerators/LanguageEnumerator.cs
+++ Assets/Scripts/Misc/Settings/OptionEnumerators/LanguageEnumerator.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static Codice.CM.Common.CmCallContext;
 
 namespace MajdataPlay.Settings.OptionEnumerators;
 public sealed class LanguageEnumerator : OptionEnumeratorBase, IOptionEnumerator
