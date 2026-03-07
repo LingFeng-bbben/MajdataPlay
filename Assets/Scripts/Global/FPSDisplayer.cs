90b30de631f45355ee69072ef6e6e3024a261e41
LeZi9916 <lezi@leziblog.com>
2026-03-08 00:07:34 +0800

    fix: fps displayer

--- Assets/Scripts/Global/FPSDisplayer.cs
+++ Assets/Scripts/Global/FPSDisplayer.cs
@@ -14,6 +14,8 @@ namespace MajdataPlay
 {
     internal sealed class FPSDisplayer : MajSingleton
     {
+        const int FPS_SAMPLE_COUNT = 120;
+        const int _1_LOW_FPS_SAMPLE_COUNT = 120;
         public static Color BgColor { get; set; } = new Color(0, 0, 0);
 
         uint _avgFPSIndex = 0;
@@ -21,19 +23,22 @@ namespace MajdataPlay
         uint _1_lowFrameSampleCount = 0;
 
         float _frameTimer = 1;
-        float _totalFrameTimeSec = 0;
+        long _totalFrameTimeTicks = 0;
 
-        readonly float[] _avgFPSData = new float[150];
-        readonly (float FrameTimeSec, ulong FrameIndex)[] _1_lowFrameData = new (float FrameTimeSec, ulong FrameIndex)[1500];
+        readonly long[] _avgFPSData = new long[FPS_SAMPLE_COUNT];
+        readonly (long FrameTimeTicks, ulong FrameIndex)[] _1_lowFrameData = new (long FrameTimeTicks, ulong FrameIndex)[_1_LOW_FPS_SAMPLE_COUNT];
 
         TextMeshPro _textDisplayer;
         GameSetting _setting;
+
+        TimeSpan _lastUpdateTiming = TimeSpan.Zero;
 
         protected override void Awake()
         {
             base.Awake();
             MajInstances.FPSDisplayer = this;
             _textDisplayer = GetComponent<TextMeshPro>();
+            _lastUpdateTiming = MajTimeline.UnscaledTime;
         }
         internal void Init()
         {
@@ -44,14 +49,16 @@ namespace MajdataPlay
         [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
         void LateUpdate()
         {
-            var delta = MajTimeline.DeltaTime;
-            AddSample(delta);
+            var currentTime = MajTimeline.UnscaledTime;
+            var delta = currentTime - _lastUpdateTiming;
+            AddSample(delta.Ticks);
+            _lastUpdateTiming = currentTime;
             if (_frameTimer <= 0)
             {
                 _textDisplayer.enabled = _setting.Debug.DisplayFPS;
-                var avgFPS = _totalFrameTimeSec / _avgFPSSampleCount;
+                var avgFPS = (TimeSpan.FromTicks(_totalFrameTimeTicks) / _avgFPSSampleCount).TotalSeconds;
                 using var sb = ZString.CreateStringBuilder(true);
-                if (_1_lowFrameSampleCount != 1500)
+                if (_1_lowFrameSampleCount != _1_LOW_FPS_SAMPLE_COUNT)
                 {
                     sb.AppendFormat("FPS  {0:F2}   1%  --.--", 1 / avgFPS);
                     var a = sb.AsArraySegment();
@@ -59,13 +66,14 @@ namespace MajdataPlay
                 }
                 else
                 {
-                    var totalLowFrameTime = 0f;
-                    for (var i = 0U; i < 150; i++)
+                    var totalLowFrameTime = 0L;
+                    var sampleCount = (int)(_1_LOW_FPS_SAMPLE_COUNT * 0.01);
+                    for (var i = 0U; i < sampleCount; i++)
                     {
                         ref var data = ref _1_lowFrameData[i];
-                        totalLowFrameTime += data.FrameTimeSec;
+                        totalLowFrameTime += data.FrameTimeTicks;
                     }
-                    var avgLowFrameTime = totalLowFrameTime / 150; 
+                    var avgLowFrameTime = (TimeSpan.FromTicks(totalLowFrameTime) / sampleCount).TotalSeconds; 
                     sb.AppendFormat("FPS  {0:F2}   1%  {1:F2}", 1 / avgFPS, 1 / avgLowFrameTime);
                     var a = sb.AsArraySegment();
                     _textDisplayer.SetCharArray(a.Array, a.Offset, a.Count);
@@ -74,31 +82,31 @@ namespace MajdataPlay
             }
             else
             {
-                _frameTimer -= delta;
+                _frameTimer -= (float)delta.TotalSeconds;
             }
         }
         [Il2CppSetOption(Option.NullChecks, false)]
         [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        unsafe void AddSample(in float data)
+        unsafe void AddSample(in long frameTicks)
         {
-            if (_avgFPSIndex >= 150)
+            if (_avgFPSIndex >= FPS_SAMPLE_COUNT)
             {
                 _avgFPSIndex = 0;
             }
-            if (_avgFPSSampleCount != 150)
+            if (_avgFPSSampleCount != FPS_SAMPLE_COUNT)
             {
                 _avgFPSSampleCount++;
             }
             ref var lastSample = ref _avgFPSData[_avgFPSIndex++];
-            _totalFrameTimeSec -= lastSample;
-            _totalFrameTimeSec += data;
-            lastSample = data;
+            _totalFrameTimeTicks -= lastSample;
+            _totalFrameTimeTicks += frameTicks;
+            lastSample = frameTicks;
             
-            fixed((float FrameTime, ulong FrameIndex)* lowFrameDataPtr = _1_lowFrameData)
+            fixed((long FrameTimeTicks, ulong FrameIndex)* lowFrameDataPtr = _1_lowFrameData)
             {
                 const int BYTES_SIZE = 16;
-                if (_1_lowFrameSampleCount < 1500)
+                if (_1_lowFrameSampleCount < _1_LOW_FPS_SAMPLE_COUNT)
                 {
                     _1_lowFrameSampleCount++;
                 }
@@ -107,7 +115,7 @@ namespace MajdataPlay
                 var thisFrameIndex = MajTimeline.FrameCount;
                 var oldestFrameIndex = ulong.MaxValue;
                 var oldestRecordIndex = -1;
-                for (var i = 0; i < 1500; i++)
+                for (var i = 0; i < _1_LOW_FPS_SAMPLE_COUNT; i++)
                 {
                     ref var data2 = ref *(lowFrameDataPtr + i);
                     
@@ -115,18 +123,18 @@ namespace MajdataPlay
                     {
                         case 0:
                             {
-                                if (data2.FrameTime == 0)
+                                if (data2.FrameTimeTicks == 0)
                                 {
-                                    data2.FrameTime = data;
+                                    data2.FrameTimeTicks = frameTicks;
                                     data2.FrameIndex = thisFrameIndex;
                                     return;
                                 }
-                                else if (data2.FrameTime < data)
+                                else if (data2.FrameTimeTicks < frameTicks)
                                 {
                                     //Array.Copy(_1_lowFrameData, i, _1_lowFrameData, i + 1, 1500 - i - 1);
-                                    var bytes2Copy = (1500 - i - 1) * BYTES_SIZE;
+                                    var bytes2Copy = (_1_LOW_FPS_SAMPLE_COUNT - i - 1) * BYTES_SIZE;
                                     Buffer.MemoryCopy(lowFrameDataPtr + i, lowFrameDataPtr + i + 1, bytes2Copy, bytes2Copy);
-                                    data2.FrameTime = data;
+                                    data2.FrameTimeTicks = frameTicks;
                                     data2.FrameIndex = thisFrameIndex;
                                     flag = 1;
                                     continue;
@@ -136,7 +144,7 @@ namespace MajdataPlay
                             }
                         case 1:
                             {
-                                if (data2.FrameTime == 0)
+                                if (data2.FrameTimeTicks == 0)
                                 {
                                     return;
                                 }
@@ -156,9 +164,9 @@ namespace MajdataPlay
                 }
                 if(oldestRecordIndex != -1)
                 {
-                    var bytes2Copy = (1500 - oldestRecordIndex - 1) * BYTES_SIZE;
+                    var bytes2Copy = (_1_LOW_FPS_SAMPLE_COUNT - oldestRecordIndex - 1) * BYTES_SIZE;
                     Buffer.MemoryCopy(lowFrameDataPtr + oldestRecordIndex + 1, lowFrameDataPtr + oldestRecordIndex, bytes2Copy, bytes2Copy);
-                    *(lowFrameDataPtr + 1499) = (0 , 0);
+                    *(lowFrameDataPtr + (_1_LOW_FPS_SAMPLE_COUNT - 1)) = (0 , 0);
                 }
             }
         }
