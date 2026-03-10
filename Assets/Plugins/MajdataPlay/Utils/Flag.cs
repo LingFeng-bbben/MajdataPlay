2ccc354e7b93fba94d225651c7340b680adbe7a9
LeZi9916 <lezi@leziblog.com>
2026-03-10 20:13:04 +0800

    feat: flag

---
+++ Assets/Plugins/MajdataPlay/Utils/Flag.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+#nullable enable
+namespace MajdataPlay.Utils;
+public class Flag<T>
+{
+    public T? Value = default(T);
+}
