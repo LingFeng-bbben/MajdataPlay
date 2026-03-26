0c157fc1fede8276234885ff5882d0f172c0ebbc
LeZi9916 <lezi@leziblog.com>
2026-03-26 17:11:29 +0800

    chore(android): hack

--- Assets/Scripts/Editor/BuildProcessor.cs
+++ Assets/Scripts/Editor/BuildProcessor.cs
@@ -2,18 +2,39 @@
 using MajdataPlay.Editor.Android;
 using System.Collections.Generic;
 using System.IO;
+using UnityEditor;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
 using UnityEngine;
 
 namespace MajdataPlay.Editor;
-class BuildProcessor : IPreprocessBuildWithReport
+class BuildProcessor : IPreprocessBuildWithReport, IPostprocessBuildWithReport
 {
     public int callbackOrder { get { return 0; } }
+
+    static AndroidSdkVersions? _originSdkVersion;
     public void OnPreprocessBuild(BuildReport report)
     {
         Debug.LogWarning("OnPreprocessBuild");
 
+        if (report.summary.platform == BuildTarget.Android && EditorUserBuildSettings.exportAsGoogleAndroidProject)
+        {
+            _originSdkVersion = PlayerSettings.Android.targetSdkVersion;
+            PlayerSettings.Android.targetSdkVersion = AndroidSdkVersions.AndroidApiLevel36;
+        }
+
         AndroidProcessor.OnPreprocessBuild(report);
+    }
+
+    public void OnPostprocessBuild(BuildReport report)
+    {
+        Debug.LogWarning("OnPostprocessBuild");
+
+        if (_originSdkVersion is AndroidSdkVersions originSdkVersion)
+        {
+            PlayerSettings.Android.targetSdkVersion = originSdkVersion;
+            _originSdkVersion = null;
+            AssetDatabase.SaveAssets();
+        }
     }
 }
