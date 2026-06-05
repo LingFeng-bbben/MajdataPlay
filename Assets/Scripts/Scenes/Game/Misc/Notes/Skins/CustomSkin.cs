c024dd0294616b1b995853ca4d614d3ccacba82e
LeZi9916 <lezi@leziblog.com>
2026-06-05 22:59:39 +0800

    feat: add skin for mine note guide and mine hold end point

--- Assets/Scripts/Scenes/Game/Misc/Notes/Skins/CustomSkin.cs
+++ Assets/Scripts/Scenes/Game/Misc/Notes/Skins/CustomSkin.cs
@@ -145,6 +145,7 @@ namespace MajdataPlay.Scenes.Game.Notes.Skins
         public Sprite TapLine_Each { get; private set; }
         public Sprite TapLine_Slide { get; private set; }
         public Sprite TapLine_Break { get; private set; }
+        public Sprite TapLine_Mine { get; private set; }
 
         public Sprite[] EachLines { get; private set; } = new Sprite[4];
         public Sprite HoldEndPoint_Normal { get; private set; }
@@ -1178,6 +1179,7 @@ namespace MajdataPlay.Scenes.Game.Notes.Skins
                 TapLine_Each = SpriteLoader.Load(skinCollectionPath + "/NoteGuideSkins/Each.png");
                 TapLine_Slide = SpriteLoader.Load(skinCollectionPath + "/NoteGuideSkins/Slide.png");
                 TapLine_Break = SpriteLoader.Load(skinCollectionPath + "/NoteGuideSkins/Break.png");
+                TapLine_Mine = SpriteLoader.Load(skinCollectionPath + "/NoteGuideSkins/Mine.png");
 
                 EachLines[0] = SpriteLoader.Load(skinCollectionPath + "/NoteGuideSkins/EachLine1.png");
                 EachLines[1] = SpriteLoader.Load(skinCollectionPath + "/NoteGuideSkins/EachLine2.png");
@@ -1187,6 +1189,7 @@ namespace MajdataPlay.Scenes.Game.Notes.Skins
                 HoldEndPoint_Normal = SpriteLoader.Load(skinCollectionPath + "/NoteGuideSkins/Hold_End.png");
                 HoldEndPoint_Each = SpriteLoader.Load(skinCollectionPath + "/NoteGuideSkins/Hold_Each_End.png");
                 HoldEndPoint_Break = SpriteLoader.Load(skinCollectionPath + "/NoteGuideSkins/Hold_Break_End.png");
+                HoldEndPoint_Break = SpriteLoader.Load(skinCollectionPath + "/NoteGuideSkins/Hold_Mine_End.png");
 
                 IsLoaded = true;
             }
