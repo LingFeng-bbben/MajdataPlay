59b1dd46cc3e1a315414584e4c826284d857adec
LeZi9916 <lezi@leziblog.com>
2026-07-16 14:00:06 +0800

    perf

--- Assets/Plugins/MajdataPlay/Drawing/SpriteLoader.cs
+++ Assets/Plugins/MajdataPlay/Drawing/SpriteLoader.cs
@@ -61,6 +61,8 @@ namespace MajdataPlay.Drawing
             try
             {
                 var texture = Decode(data, markNonReadable);
+                //var texture = new Texture2D(0, 0);
+                //texture.LoadImage(data, markNonReadable);
 
                 return Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f), 100, 1,
                     SpriteMeshType.FullRect, border);
