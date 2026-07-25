a876e10b2086c627d47ec2ff62ab6eb0e3651e6a
LeZi9916 <lezi@leziblog.com>
2026-07-25 15:57:09 +0800

    feat: ListFilteredInicator

---
+++ Assets/Scripts/Misc/UI/Inicators/ListFilteredInicator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MajdataPlay.UI.Inicators
+{
+    public class ListFilteredInicator : UIInicator
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+            var actionTo = ActionTo;
+            if (actionTo == null)
+            {
+                actionTo = gameObject;
+            }
+            actionTo.SetActive(!string.IsNullOrEmpty(MajEnv.RuntimeConfig.List.OrderBy.Keyword));
+        }
+    }
+}
