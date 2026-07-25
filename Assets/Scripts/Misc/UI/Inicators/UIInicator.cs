6fb1bf8197514d3ff0d2869afbc86cf8a6713df1
LeZi9916 <lezi@leziblog.com>
2026-07-25 15:54:06 +0800

    feat: UIInicator

---
+++ Assets/Scripts/Misc/UI/Inicators/UIInicator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace MajdataPlay.UI.Inicators
+{
+    public abstract class UIInicator : MajBehaviour
+    {
+        [SerializeField]
+        [FormerlySerializedAs("actionTo")]
+        protected GameObject ActionTo;
+    }
+}
