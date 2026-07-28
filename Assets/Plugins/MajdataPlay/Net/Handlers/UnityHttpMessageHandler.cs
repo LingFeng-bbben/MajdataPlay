782a7783437b7b9724272913f18f996ead25e385
LeZi9916 <lezi@leziblog.com>
2026-07-28 21:57:14 +0800

    feat: UnityHttpMessageHandler

---
+++ Assets/Plugins/MajdataPlay/Net/Handlers/UnityHttpMessageHandler.cs
@@ -0,0 +1,140 @@
+﻿using Cysharp.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine.Networking;
+#nullable enable
+namespace MajdataPlay.Net.Handlers
+{
+    public class UnityHttpMessageHandler : HttpMessageHandler
+    {
+        public int MaxRedirects { get; set; } = 32;
+
+
+        protected override async Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            await using (UniTask.ReturnToCurrentSynchronizationContext(true, cancellationToken))
+            {
+                await UniTask.SwitchToMainThread(cancellationToken);
+                using var uwr = await CreateWebRequest(request);
+
+                var operation = uwr.SendWebRequest();
+                await operation.WithCancellation(cancellationToken, true);
+                await UniTask.SwitchToThreadPool();
+
+                var response = new HttpResponseMessage((HttpStatusCode)uwr.responseCode);
+
+                if (uwr.downloadHandler != null)
+                {
+                    response.Content = new DownloadHandlerHttpContent(uwr.downloadHandler);
+                }
+
+                foreach (var kv in uwr.GetResponseHeaders())
+                {
+                    response.Headers.TryAddWithoutValidation(kv.Key, kv.Value);
+                }
+
+                response.RequestMessage = request;
+                response.ReasonPhrase = uwr.error;
+                return response;
+            }
+        }
+
+
+        async Task<UnityWebRequest> CreateWebRequest(HttpRequestMessage request)
+        {
+            var uwr = default(UnityWebRequest);
+            var method = request.Method.Method;
+
+            if (request.Content != null)
+            {
+                var contentLength = request.Content.Headers.ContentLength ?? 0;
+                var body = await request.Content.ReadAsByteArrayAsync();
+                var buffer = new NativeArray<byte>((int)contentLength, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
+                buffer.CopyFrom(body);
+
+                uwr = new UnityWebRequest(request.RequestUri, method)
+                {
+                    uploadHandler = new UploadHandlerRaw(buffer, true),
+                    downloadHandler = new DownloadHandlerBuffer()
+                };
+                foreach (var header in request.Content.Headers)
+                {
+                    foreach (var value in header.Value)
+                    {
+                        uwr.SetRequestHeader(header.Key, value);
+                    }
+                }
+            }
+            else
+            {
+                uwr = new UnityWebRequest(request.RequestUri, method)
+                {
+                    downloadHandler = new DownloadHandlerBuffer()
+                };
+            }
+
+            uwr.redirectLimit = MaxRedirects;
+            uwr.timeout = (int)UnityWebRequestFactory.Timeout.TotalMilliseconds / 1000;
+            uwr.SetRequestHeader("User-Agent", UnityWebRequestFactory.UserAgent);
+
+            foreach (var header in request.Headers)
+            {
+                foreach (var value in header.Value)
+                {
+                    uwr.SetRequestHeader(header.Key, value);
+                }
+            }
+
+            return uwr;
+        }
+        class DownloadHandlerHttpContent : HttpContent
+        {
+            readonly DownloadHandler _handler;
+            readonly NativeArray<byte>.ReadOnly _data;
+
+            public DownloadHandlerHttpContent(DownloadHandler handler)
+            {
+                _handler = handler;
+                _data = handler.nativeData;
+
+                Headers.ContentLength = _data.Length;
+            }
+
+
+            protected override unsafe Task SerializeToStreamAsync(
+                Stream stream,
+                TransportContext? context)
+            {
+                if (!_data.IsCreated || _data.Length == 0)
+                {
+                    return Task.CompletedTask;
+                }
+
+
+                var memory = new UnmanagedMemoryStream(
+                    (byte*)_data.GetUnsafeReadOnlyPtr(),
+                    _data.Length);
+
+                return memory.CopyToAsync(stream, 81920);             
+            }
+
+            protected override bool TryComputeLength(out long length)
+            {
+                length = _data.Length;
+                return true;
+            }
+        }
+    }
+}
--- Assets/Plugins/MajdataPlay/Net/UnityWebRequestFactory.cs
+++ Assets/Plugins/MajdataPlay/Net/UnityWebRequestFactory.cs
@@ -7,79 +7,81 @@ using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Networking;
 
-namespace MajdataPlay.Net;
-public static class UnityWebRequestFactory
+namespace MajdataPlay.Net
 {
-    public static TimeSpan Timeout { get; set; } = TimeSpan.FromSeconds(2);
-    public static string UserAgent { get; set; } = "MajdataPlay/0.1.0";
-    #region Post
-    public static UnityWebRequest Post(string uri, string postData, string contentType)
+    public static class UnityWebRequestFactory
     {
-        var req = UnityWebRequest.Post(uri, postData, contentType);
-        req.timeout = (int)Timeout.TotalMilliseconds / 1000;
-        req.SetRequestHeader("User-Agent", UserAgent);
+        public static TimeSpan Timeout { get; set; } = TimeSpan.FromSeconds(2);
+        public static string UserAgent { get; set; } = "MajdataPlay/0.1.0";
+        #region Post
+        public static UnityWebRequest Post(string uri, string postData, string contentType)
+        {
+            var req = UnityWebRequest.Post(uri, postData, contentType);
+            req.timeout = (int)Timeout.TotalMilliseconds / 1000;
+            req.SetRequestHeader("User-Agent", UserAgent);
 
-        return req;
-    }
-    public static UnityWebRequest Post(string uri, WWWForm formData)
-    {
-        var req = UnityWebRequest.Post(uri, formData);
-        req.timeout = (int)Timeout.TotalMilliseconds / 1000;
-        req.SetRequestHeader("User-Agent", UserAgent);
+            return req;
+        }
+        public static UnityWebRequest Post(string uri, WWWForm formData)
+        {
+            var req = UnityWebRequest.Post(uri, formData);
+            req.timeout = (int)Timeout.TotalMilliseconds / 1000;
+            req.SetRequestHeader("User-Agent", UserAgent);
 
-        return req;
-    }
-    public static UnityWebRequest Post(Uri uri, string postData, string contentType)
-    {
-        var req = UnityWebRequest.Post(uri, postData, contentType);
-        req.timeout = (int)Timeout.TotalMilliseconds / 1000;
-        req.SetRequestHeader("User-Agent", UserAgent);
+            return req;
+        }
+        public static UnityWebRequest Post(Uri uri, string postData, string contentType)
+        {
+            var req = UnityWebRequest.Post(uri, postData, contentType);
+            req.timeout = (int)Timeout.TotalMilliseconds / 1000;
+            req.SetRequestHeader("User-Agent", UserAgent);
 
-        return req;
-    }
-    public static UnityWebRequest Post(Uri uri, WWWForm formData)
-    {
-        var req = UnityWebRequest.Post(uri, formData);
-        req.timeout = (int)Timeout.TotalMilliseconds / 1000;
-        req.SetRequestHeader("User-Agent", UserAgent);
+            return req;
+        }
+        public static UnityWebRequest Post(Uri uri, WWWForm formData)
+        {
+            var req = UnityWebRequest.Post(uri, formData);
+            req.timeout = (int)Timeout.TotalMilliseconds / 1000;
+            req.SetRequestHeader("User-Agent", UserAgent);
         
-        return req;
-    }
-    #endregion
-    #region Get
-    public static UnityWebRequest Get(string uri)
-    {
-        var req = UnityWebRequest.Get(uri);
-        req.timeout = (int)Timeout.TotalMilliseconds / 1000;
-        req.SetRequestHeader("User-Agent", UserAgent);
+            return req;
+        }
+        #endregion
+        #region Get
+        public static UnityWebRequest Get(string uri)
+        {
+            var req = UnityWebRequest.Get(uri);
+            req.timeout = (int)Timeout.TotalMilliseconds / 1000;
+            req.SetRequestHeader("User-Agent", UserAgent);
 
-        return req;
-    }
-    public static UnityWebRequest Get(Uri uri)
-    {
-        var req = UnityWebRequest.Get(uri);
-        req.timeout = (int)Timeout.TotalMilliseconds / 1000;
-        req.SetRequestHeader("User-Agent", UserAgent);
+            return req;
+        }
+        public static UnityWebRequest Get(Uri uri)
+        {
+            var req = UnityWebRequest.Get(uri);
+            req.timeout = (int)Timeout.TotalMilliseconds / 1000;
+            req.SetRequestHeader("User-Agent", UserAgent);
 
-        return req;
-    }
-    #endregion
-    #region Head
-    public static UnityWebRequest Head(string uri)
-    {
-        var req = UnityWebRequest.Head(uri);
-        req.timeout = (int)Timeout.TotalMilliseconds / 1000;
-        req.SetRequestHeader("User-Agent", UserAgent);
+            return req;
+        }
+        #endregion
+        #region Head
+        public static UnityWebRequest Head(string uri)
+        {
+            var req = UnityWebRequest.Head(uri);
+            req.timeout = (int)Timeout.TotalMilliseconds / 1000;
+            req.SetRequestHeader("User-Agent", UserAgent);
 
-        return req;
-    }
-    public static UnityWebRequest Head(Uri uri)
-    {
-        var req = UnityWebRequest.Head(uri);
-        req.timeout = (int)Timeout.TotalMilliseconds / 1000;
-        req.SetRequestHeader("User-Agent", UserAgent);
+            return req;
+        }
+        public static UnityWebRequest Head(Uri uri)
+        {
+            var req = UnityWebRequest.Head(uri);
+            req.timeout = (int)Timeout.TotalMilliseconds / 1000;
+            req.SetRequestHeader("User-Agent", UserAgent);
 
-        return req;
+            return req;
+        }
+        #endregion
     }
-    #endregion
 }
