ffd7c793dea9aaf5caedffecc95ae1042c84b9bc
LeZi9916 <lezi@leziblog.com>
2026-07-29 10:33:07 +0800

    feat: CanvasConfigurator

---
+++ Assets/Scripts/Misc/UI/CanvasConfigurator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace MajdataPlay.UI
+{
+    [RequireComponent(typeof(Canvas))]
+    public class CanvasConfigurator : MonoBehaviour
+    {
+        Canvas _canvas;
+
+        void Awake()
+        {
+            _canvas = GetComponent<Canvas>();
+            _canvas.worldCamera = Camera.main;
+        }
+    }
+}
