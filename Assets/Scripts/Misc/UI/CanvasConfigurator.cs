fa7b1030d290fdb4704c7c3845986c27eeb28e70
LeZi9916 <lezi@leziblog.com>
2026-07-29 10:44:19 +0800

    perf: add CanvasConfigurator to CanvasTemplate

--- Assets/Scripts/Misc/UI/CanvasConfigurator.cs
+++ Assets/Scripts/Misc/UI/CanvasConfigurator.cs
@@ -4,18 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
-
+#nullable enable
 namespace MajdataPlay.UI
 {
     [RequireComponent(typeof(Canvas))]
     public class CanvasConfigurator : MonoBehaviour
     {
         Canvas _canvas;
+        SceneSwitcher _sceneSwitcher;
 
         void Awake()
         {
             _canvas = GetComponent<Canvas>();
-            _canvas.worldCamera = Camera.main;
+            _sceneSwitcher = MajInstances.SceneSwitcher;
+            _canvas.worldCamera = _sceneSwitcher.MainCamera;
+            SceneSwitcher.OnSceneChanged += OnSceneChanged;
+        }
+
+        void OnDestroy()
+        {
+            SceneSwitcher.OnSceneChanged -= OnSceneChanged;
+        }
+
+        void OnSceneChanged(object? sender, (MajScenes NewScene, MajScenes OldScene) args)
+        {
+            _canvas.worldCamera = _sceneSwitcher.MainCamera;
         }
     }
 }
