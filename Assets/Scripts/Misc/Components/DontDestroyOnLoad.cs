e7a4bd86a3ae1d8444c15202b4f5594f0177b04a
LeZi9916 <lezi@leziblog.com>
2026-07-29 10:48:26 +0800

    feat: DontDestroyOnLoad

---
+++ Assets/Scripts/Misc/Components/DontDestroyOnLoad.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace MajdataPlay.Components
+{
+    public class DontDestroyOnLoad : MonoBehaviour
+    {
+        [SerializeField]
+        [FormerlySerializedAs("dontDestroyOnLoad")]
+        bool _dontDestroyOnLoad;
+
+        void Awake()
+        {
+            if (_dontDestroyOnLoad)
+            {
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+    }
+}
