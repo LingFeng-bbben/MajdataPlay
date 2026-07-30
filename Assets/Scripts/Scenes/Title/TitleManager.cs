ec533a4bcde37bdb6a3b3c260095fa84433794f5
anqtal <osiom@outlook.com>
2026-07-30 23:47:08 +1000

    feat: title animation

--- Assets/Scripts/Scenes/Title/TitleManager.cs
+++ Assets/Scripts/Scenes/Title/TitleManager.cs
@@ -1,32 +1,76 @@
 using Cysharp.Threading.Tasks;
+using LitMotion;
+using LitMotion.Extensions;
 using MajdataPlay.Settings;
 using MajdataPlay.Extensions;
 using MajdataPlay.IO;
 using MajdataPlay.Utils;
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Text.RegularExpressions;
-using System.Threading;
 using System.Threading.Tasks;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Networking;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 #nullable enable
 namespace MajdataPlay.Scenes.Title
 {
     public class TitleManager : MonoBehaviour
     {
-        public TextMeshProUGUI echoText;
-        public Animator fadeInAnim;
+        [SerializeField]
+        RectTransform _xxlbRect = null!;
+
+        [SerializeField]
+        Image _xxlbImage = null!;
+
+        [SerializeField]
+        RectTransform _majdataPlayRect = null!;
+
+        [SerializeField]
+        Image _majdataPlayImage = null!;
+
+        [SerializeField]
+        Image _ribbonImage = null!;
+
+        [SerializeField]
+        RectTransform _echoBackground = null!;
+
+        [SerializeField]
+        TextMeshProUGUI _echoText = null!;
+
+        [SerializeField]
+        RectTransform _loadingIndicator = null!;
 
         bool _flag = false;
-        float _pressTime = 0f;
+
+        readonly CompositeMotionHandle _titleEntranceMotions = new(5);
+        MotionHandle _loadingRotationMotion;
+        float _xxlbTargetX;
+        float _majdataPlayTargetX;
+        float _echoBackgroundTargetWidth;
+        bool _isInitializationDone;
+
+        const float XXLB_ENTRANCE_DURATION_SEC = 80f / 60f;
+        const float XXLB_START_OFFSET_X = -540f;
+        const float MAJDATA_PLAY_START_OFFSET_X = 540f;
+        const float RIBBON_REVEAL_DELAY_SEC = XXLB_ENTRANCE_DURATION_SEC;
+        const float RIBBON_REVEAL_DURATION_SEC = 27f / 60f;
+        const float ECHO_BACKGROUND_DELAY_SEC = 102f / 60f;
+        const float ECHO_BACKGROUND_DURATION_SEC = 23f / 60f;
+        const float ECHO_TEXT_DELAY_SEC = 122f / 60f;
+        const float ECHO_TEXT_FADE_DURATION_SEC = 20f / 60f;
+        const float LOADING_ROTATION_DURATION_SEC = 3f;
+
+        void Awake()
+        {
+            _xxlbTargetX = _xxlbRect.anchoredPosition.x;
+            _majdataPlayTargetX = _majdataPlayRect.anchoredPosition.x;
+            _echoBackgroundTargetWidth = _echoBackground.sizeDelta.x;
+            PrepareTitleEntrance();
+        }
+
         void Start()
         {
+            PlayTitleEntranceAnimation();
             InitAsync().Forget();
             CabinetLed.SetAllLight(Color.white);
             if (InputManager.IsTouchPanelConnected)
@@ -40,9 +84,7 @@ namespace MajdataPlay.Scenes.Title
             MajInstances.AudioManager.PlaySFX("MajdataPlay.wav");
             MajInstances.AudioManager.PlaySFX("bgm_title.mp3");
 
-
-
-            echoText.text = $"{"MAJTEXT_LOADING_SCORE_STORAGE".i18n()}...";
+            _echoText.text = $"{"MAJTEXT_LOADING_SCORE_STORAGE".i18n()}...";
             await UniTask.DelayFrame(9);
             var task1 = ScoreManager.InitAsync().AsValueTask();
             while (!task1.IsCompleted)
@@ -57,22 +99,17 @@ namespace MajdataPlay.Scenes.Title
                 await UniTask.Yield();
             }
             await UniTask.Delay(2000);
-            echoText.text = $"{"MAJTEXT_LOADING_SKIN".i18n()}...";
+            _echoText.text = $"{"MAJTEXT_LOADING_SKIN".i18n()}...";
             var task2 = MajInstances.SkinManager.InitAsync();
             while (!task2.IsCompleted)
             {
                 await UniTask.Yield();
             }
 
-            echoText.text = $"{"MAJTEXT_SCANNING_CHARTS".i18n()}...";
+            _echoText.text = $"{"MAJTEXT_SCANNING_CHARTS".i18n()}...";
             var task3 = StartScanningChart();
             try
             {
-                if (task3 is null)
-                {
-                    return;
-                }
-                var isEmpty = false;
                 while (true)
                 {
                     await UniTask.Yield(PlayerLoopTiming.Update);
@@ -81,17 +118,12 @@ namespace MajdataPlay.Scenes.Title
                     {
                         if (task3.IsFaulted)
                         {
-                            echoText.text = "MAJTEXT_ERR_SCAN_CHARTS_FAILED".i18n();
+                            _echoText.text = "MAJTEXT_ERR_SCAN_CHARTS_FAILED".i18n();
                             MajDebug.LogException(task3.Exception);
                         }
-                        //else if (SongStorage.IsEmpty)
-                        //{
-                        //    isEmpty = true;
-                        //    echoText.text = "MAJTEXT_ERR_NO_CHART".i18n();
-                        //}
                         else
                         {
-                            echoText.text = "MAJTEXT_PRESS_ANY_KEY".i18n();
+                            _echoText.text = "MAJTEXT_PRESS_ANY_KEY".i18n();
                             InputManager.BindAnyArea(OnAreaDown);
 
                             var list = new string[] { "game_init.wav", "game_init_2.wav", "game_init_3.wav" };
@@ -101,7 +133,7 @@ namespace MajdataPlay.Scenes.Title
                         break;
                     }
                 }
-                fadeInAnim.SetBool("IsDone", true);
+                FinishLoadingAnimation();
             }
             finally
             {
@@ -114,14 +146,13 @@ namespace MajdataPlay.Scenes.Title
             var progress = new Progress<string>();
             progress.ProgressChanged += (o, e) =>
             {
-                echoText.text = e;
+                _echoText.text = e;
             };
             await Task.Delay(3000);
             await SongStorage.InitAsync(progress);
 
             if (!SongStorage.IsEmpty)
             {
-                var setting = MajEnv.Settings;
                 var listConfig = MajEnv.RuntimeConfig.List;
                 var dirId = listConfig.SelectedDirGuid;
                 var selectedSongHash = listConfig.SelectedSongHash;
@@ -168,6 +199,122 @@ namespace MajdataPlay.Scenes.Title
             }
         }
 
+        void PrepareTitleEntrance()
+        {
+            _titleEntranceMotions.Cancel();
+            _loadingRotationMotion.TryCancel();
+            SetXxlbPositionX(_xxlbTargetX + XXLB_START_OFFSET_X);
+            SetGraphicAlpha(_xxlbImage, 0f);
+            SetMajdataPlayPositionX(_majdataPlayTargetX + MAJDATA_PLAY_START_OFFSET_X);
+            SetGraphicAlpha(_majdataPlayImage, 0f);
+            _ribbonImage.type = Image.Type.Filled;
+            _ribbonImage.fillMethod = Image.FillMethod.Horizontal;
+            _ribbonImage.fillOrigin = (int)Image.OriginHorizontal.Left;
+            _ribbonImage.fillAmount = 0f;
+            SetGraphicAlpha(_ribbonImage, 1f);
+            SetGraphicAlpha(_echoText, 0f);
+            SetEchoBackgroundWidth(0f);
+            _loadingIndicator.localEulerAngles = Vector3.zero;
+            _loadingIndicator.gameObject.SetActive(false);
+        }
+
+        void PlayTitleEntranceAnimation()
+        {
+            LMotion.Create(0f, 1f, XXLB_ENTRANCE_DURATION_SEC)
+                   .WithEase(Ease.OutQuart)
+                   .Bind(progress =>
+                   {
+                       SetXxlbPositionX(Mathf.LerpUnclamped(
+                           _xxlbTargetX + XXLB_START_OFFSET_X,
+                           _xxlbTargetX,
+                           progress));
+                       SetGraphicAlpha(_xxlbImage, progress);
+                   })
+                   .AddTo(_titleEntranceMotions);
+
+            LMotion.Create(0f, 1f, XXLB_ENTRANCE_DURATION_SEC)
+                   .WithEase(Ease.OutQuart)
+                   .Bind(progress =>
+                   {
+                       SetMajdataPlayPositionX(Mathf.LerpUnclamped(
+                           _majdataPlayTargetX + MAJDATA_PLAY_START_OFFSET_X,
+                           _majdataPlayTargetX,
+                           progress));
+                       SetGraphicAlpha(_majdataPlayImage, progress);
+                   })
+                   .AddTo(_titleEntranceMotions);
+
+            LMotion.Create(0f, 1f, RIBBON_REVEAL_DURATION_SEC)
+                   .WithDelay(RIBBON_REVEAL_DELAY_SEC)
+                   .WithEase(Ease.InOutSine)
+                   .BindToFillAmount(_ribbonImage)
+                   .AddTo(_titleEntranceMotions);
+
+            LMotion.Create(0f, _echoBackgroundTargetWidth, ECHO_BACKGROUND_DURATION_SEC)
+                   .WithDelay(ECHO_BACKGROUND_DELAY_SEC)
+                   .WithEase(Ease.OutBack)
+                   .Bind(SetEchoBackgroundWidth)
+                   .AddTo(_titleEntranceMotions);
+
+            LMotion.Create(0f, 1f, ECHO_TEXT_FADE_DURATION_SEC)
+                   .WithDelay(ECHO_TEXT_DELAY_SEC)
+                   .WithEase(Ease.InOutSine)
+                   .WithOnComplete(StartLoadingAnimation)
+                   .Bind(alpha => SetGraphicAlpha(_echoText, alpha))
+                   .AddTo(_titleEntranceMotions);
+        }
+
+        void StartLoadingAnimation()
+        {
+            if (_isInitializationDone)
+            {
+                return;
+            }
+
+            _loadingIndicator.gameObject.SetActive(true);
+            _loadingRotationMotion.TryCancel();
+            _loadingRotationMotion = LMotion.Create(0f, -360f, LOADING_ROTATION_DURATION_SEC)
+                                            .WithEase(Ease.Linear)
+                                            .WithLoops(-1)
+                                            .BindToLocalEulerAnglesZ(_loadingIndicator);
+        }
+
+        void FinishLoadingAnimation()
+        {
+            _isInitializationDone = true;
+            _loadingRotationMotion.TryCancel();
+            _loadingIndicator.localEulerAngles = Vector3.zero;
+            _loadingIndicator.gameObject.SetActive(false);
+        }
+
+        void SetXxlbPositionX(float x)
+        {
+            var position = _xxlbRect.anchoredPosition;
+            position.x = x;
+            _xxlbRect.anchoredPosition = position;
+        }
+
+        void SetMajdataPlayPositionX(float x)
+        {
+            var position = _majdataPlayRect.anchoredPosition;
+            position.x = x;
+            _majdataPlayRect.anchoredPosition = position;
+        }
+
+        void SetEchoBackgroundWidth(float width)
+        {
+            var size = _echoBackground.sizeDelta;
+            size.x = width;
+            _echoBackground.sizeDelta = size;
+        }
+
+        static void SetGraphicAlpha(Graphic graphic, float alpha)
+        {
+            var color = graphic.color;
+            color.a = alpha;
+            graphic.color = color;
+        }
+
         private void OnAreaDown(object sender, InputEventArgs e)
         {
             if (!e.IsDown)
@@ -210,7 +357,6 @@ namespace MajdataPlay.Scenes.Title
         void NextScene()
         {
             InputManager.UnbindAnyArea(OnAreaDown);
-            _pressTime = 0;
             _flag = false;
             MajInstances.AudioManager.StopSFX("bgm_title.mp3");
             MajInstances.AudioManager.StopSFX("MajdataPlay.wav");
@@ -222,6 +368,12 @@ namespace MajdataPlay.Scenes.Title
             {
                 MajInstances.SceneSwitcher.SwitchScene("List", false);
             }
+        }
+
+        void OnDestroy()
+        {
+            _titleEntranceMotions.Cancel();
+            _loadingRotationMotion.TryCancel();
         }
     }
 }
--- Assets/Scripts/Scenes/Title/XxlbBlink.cs
+++ Assets/Scripts/Scenes/Title/XxlbBlink.cs
@@ -1,10 +1,9 @@
 using Cysharp.Threading.Tasks;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
 using LitMotion;
 using LitMotion.Extensions;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace MajdataPlay.Scenes.Title
 {
@@ -16,29 +15,23 @@ namespace MajdataPlay.Scenes.Title
         public Sprite XxlbWink;
         public Image Image;
 
-        void Awake()
-        {
-//#if UNITY_IOS appstore njm
-//            gameObject.SetActive(false);
-//#endif
-        }
         void Start()
         {
-            Image = GetComponent<Image>();
             var rect = GetComponent<RectTransform>();
-            Winkloop().Forget();
+            WinkLoopAsync(destroyCancellationToken).Forget();
             // 起始位置
             float startY = rect.anchoredPosition.y;
             float endY = startY - 20f; // 浮动高度（可调）
 
             // 创建往返浮动动画
-            LMotion.Create(startY, endY, 2.5f)      // 1.5 秒完成一次上升
+            LMotion.Create(startY, endY, 2.5f)      // 2.5 秒完成一次下降
                 .WithEase(Ease.InOutSine)       // 平滑的上下浮动
                 .WithLoops(-1, LoopType.Yoyo)       // 无限循环 + 往返
-                .BindToAnchoredPositionY(rect);    // 绑定到物体的 Y 轴
+                .BindToAnchoredPositionY(rect)    // 绑定到物体的 Y 轴
+                .AddTo(this);
         }
 
-        async UniTaskVoid Winkloop()
+        async UniTask WinkLoopAsync(CancellationToken cancellationToken)
         {
             while (true)
             {
@@ -46,21 +39,21 @@ namespace MajdataPlay.Scenes.Title
                 if (ran <= 10)
                 {
                     Image.sprite = XxlbBlinksHalf;
-                    await UniTask.WaitForSeconds(0.1f);
+                    await UniTask.WaitForSeconds(0.1f, cancellationToken: cancellationToken);
                     Image.sprite = XxlbBlinks;
-                    await UniTask.WaitForSeconds(0.1f);
+                    await UniTask.WaitForSeconds(0.1f, cancellationToken: cancellationToken);
                     Image.sprite = XxlbDefault;
-                    await UniTask.WaitForSeconds(0.2f);
+                    await UniTask.WaitForSeconds(0.2f, cancellationToken: cancellationToken);
                 }
 
                 if (ran == 6)
                 {
                     Image.sprite = XxlbWink;
-                    await UniTask.WaitForSeconds(0.3f);
+                    await UniTask.WaitForSeconds(0.3f, cancellationToken: cancellationToken);
                     Image.sprite = XxlbDefault;
-                    await UniTask.WaitForSeconds(0.2f);
+                    await UniTask.WaitForSeconds(0.2f, cancellationToken: cancellationToken);
                 }
-                await UniTask.Yield();
+                await UniTask.Yield(cancellationToken);
             }
         }
     }
