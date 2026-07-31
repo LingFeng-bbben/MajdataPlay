6113bc7de7cdd48eecb8352e706f6f3544ea510a
LeZi9916 <lezi@leziblog.com>
2026-07-31 21:46:55 +0800

    feat: ObjectIdHelper

---
+++ Assets/Scripts/Utils/ObjectIdHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MajdataPlay.Utils
+{
+    internal static class ObjectIdHelper
+    {
+        public static Guid ToGuid(string oid)
+        {
+            Span<byte> hash = stackalloc byte[16];
+
+            using var md5 = MD5.Create();
+            var bytes = md5.ComputeHash(Encoding.UTF8.GetBytes(oid));
+
+            bytes.AsSpan(0, 16).CopyTo(hash);
+
+            return new Guid(hash);
+        }
+    }
+}
