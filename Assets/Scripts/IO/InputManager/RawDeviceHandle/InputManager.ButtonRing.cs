f3a2931e9689c7ccae7e032b62317fd48fa7da48
LeZi9916 <lezi@leziblog.com>
2026-08-01 18:09:47 +0800

    fix

--- Assets/Scripts/IO/InputManager/RawDeviceHandle/InputManager.ButtonRing.cs
+++ Assets/Scripts/IO/InputManager/RawDeviceHandle/InputManager.ButtonRing.cs
@@ -11,17 +11,15 @@ using System.Threading.Tasks;
 using UnityEngine.InputSystem;
 using UnityEngine.Profiling;
 using System.IO.Pipes;
-
+using MajdataPlay.Diagnostics;
 #if UNITY_ANDROID || UNITY_EDITOR
 using MajdataPlay.Platform.Android;
 using MajdataPlay.Platform.Android.IO;
-using MajdataPlay.Diagnostics;
-
 #endif
+
 #if UNITY_IOS || UNITY_EDITOR
 using MajdataPlay.Platform.iOS;
 #endif
-
 
 #if UNITY_STANDALONE
 using HidSharp;
