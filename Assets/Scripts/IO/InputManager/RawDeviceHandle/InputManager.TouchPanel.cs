78de1481bac7562e41dce0f2362a0b90a72c2c5a
LingFeng-bbben <51612155+LingFeng-bbben@users.noreply.github.com>
2026-08-04 15:21:16 +0800

    imsb

--- Assets/Scripts/IO/InputManager/RawDeviceHandle/InputManager.TouchPanel.cs
+++ Assets/Scripts/IO/InputManager/RawDeviceHandle/InputManager.TouchPanel.cs
@@ -801,7 +801,7 @@ namespace MajdataPlay.IO
                         //see also https://github.com/Sucareto/Mai2Touch/tree/main/Mai2Touch
 
                         serialSession.Write(encoding.GetBytes("{RSET}"));
-                        Task.Delay(3000);
+                        Thread.Sleep(3000);
                         //wait for board to reboot
                         serialSession.Write(encoding.GetBytes("{HALT}"));
 
