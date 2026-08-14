d58b6e293ad2b083ef56348812f078e227518a69
LeZi9916 <lezi@leziblog.com>
2026-08-15 01:49:12 +0800

    fix

--- Assets/Plugins/MajdataPlay/Syscall/Darwin/DarwinApi.cs
+++ Assets/Plugins/MajdataPlay/Syscall/Darwin/DarwinApi.cs
@@ -6,10 +6,10 @@ namespace MajdataPlay.Syscall.Darwin
 {
     public static class DarwinApi
     {
-#if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
-        const string DLL_NAME = "libSystem.B.dylib";
-#else
+#if UNITY_IOS
         const string DLL_NAME = "__Internal";
+#else
+        const string DLL_NAME = "libSystem.B.dylib";
 #endif
         [DllImport(DLL_NAME)]
         public static extern int pthread_threadid_np(IntPtr thread, out ulong threadId);
