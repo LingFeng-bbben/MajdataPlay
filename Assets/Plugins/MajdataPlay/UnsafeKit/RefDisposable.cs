49a7968b744101dac351d07db1b66234eee440a2
anqtal <osiom@outlook.com>
2026-08-15 23:24:31 +1000

    fix(coreclr): preserve ref disposal via spans

--- Assets/Plugins/MajdataPlay/UnsafeKit/RefDisposable.cs
+++ Assets/Plugins/MajdataPlay/UnsafeKit/RefDisposable.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,21 +10,24 @@ namespace MajdataPlay.UnsafeKit
 {
     public readonly ref struct RefDisposable
     {
-        public readonly ref struct Disposable<T> where T : IDisposable
+        public ref struct Disposable<T> where T : IDisposable
         {
-            public readonly ref T Instance;
+            private Span<T> _instance;
             public readonly bool SetNullWhenDispose;
 
             internal Disposable(ref T instance, bool setNullWhenDispose)
             {
-                Instance = ref instance;
+                _instance = MemoryMarshal.CreateSpan(ref instance, 1);
                 SetNullWhenDispose = setNullWhenDispose;
             }
 
-            public readonly void Dispose()
+            public void Dispose()
             {
-                Instance?.Dispose();
-                Instance = default;
+                _instance[0]?.Dispose();
+                if (SetNullWhenDispose)
+                {
+                    _instance[0] = default;
+                }
             }
         }
         public static Disposable<T> From<T>(ref T instance, bool setNullWhenDispose = false) where T : IDisposable
