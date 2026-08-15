52aac4a509020b60a55277618dddc32f774048fe
anqtal <osiom@outlook.com>
2026-08-15 23:26:22 +1000

    fix(coreclr): remove unsupported security policy import

--- Assets/Plugins/MajdataPlay/Drawing/SpriteLoader.cs
+++ Assets/Plugins/MajdataPlay/Drawing/SpriteLoader.cs
@@ -8,7 +8,6 @@ using System.Buffers;
 using System.IO;
 using System.Net;
 using System.Net.Http;
-using System.Security.Policy;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
