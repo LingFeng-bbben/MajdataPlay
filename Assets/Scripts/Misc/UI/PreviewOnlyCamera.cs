ba5b095cc6cdbc8899960e0ffcc2dfd1781c64fa
LeZi9916 <lezi@leziblog.com>
2026-08-20 02:01:18 +0800

    feat: PreviewOnlyCamera

---
+++ Assets/Scripts/Misc/UI/PreviewOnlyCamera.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace MajdataPlay.UI
+{
+    [RequireComponent(typeof(Camera))]
+    internal class PreviewOnlyCamera : MajComponent
+    {
+        Camera _camera;
+        protected override void Awake()
+        {
+            base.Awake();
+            _camera = GetComponent<Camera>();
+            Destroy(GameObject);
+        }
+    }
+}
